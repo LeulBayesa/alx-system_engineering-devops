ee7dac9065b65cc97b70fcffbce366105a713795
Leul Bayesa <96570532+LeulBayesa@users.noreply.github.com>
2023-04-20 23:27:42 +0300

    Delete 0x1B-web_stack_debugging_4 directory

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++
@@ -1,2 +0,0 @@
-# Changes the OS configuration so that it is possible to login with the holberton user and open a file without any error message
-exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
